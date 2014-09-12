ab1ad66ac0c02269ee20f91265a1e1ab2720528f
Cacilhας, La Batalema <batalema@cacilhas.info>
2014-09-12 13:31:04 -0300

    working application

---
+++ src/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.version = "0.9.1"
+    t.identity = "lightsout"
+    t.window.title = "LightsOut"
+    t.window.icon = "images/lightsout.png"
+    t.window.width = 160
+    t.window.height = 160
+    t.window.fullscreen = false
+end
